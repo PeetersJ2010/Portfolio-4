6de38324fb29fc928ddfab4c27a3c3324f12d983
PeetersJ2010 <r0802173@student.thomasmore.be>
2022-02-16 00:16:06 +0100

    hero

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
+
+
+
 export class NavComponent implements OnInit {
 
   constructor() { }
@@ -12,4 +15,19 @@ export class NavComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    let element = document.querySelector('.navbar') as HTMLElement;
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.remove('nav-top');
+      element.classList.add('nav-not-top');
+      console.log("GR")
+    } else {
+      element.classList.add('nav-top');
+      element.classList.remove('nav-not-top');
+    }
+  }
 }
+
+
+
